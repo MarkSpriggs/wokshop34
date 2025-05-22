e0ef6d2011acd45d9ebc797332f28a68c50fbc74
MarkSpriggs <markfspriggs@gmail.com>
2025-05-22 13:44:26 -0600

    made create and seed but npm run db seed not working

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,6 +1,10 @@
+import db from "../client.js";
+
 /** @returns the employee created according to the provided details */
 export async function createEmployee({ name, birthday, salary }) {
-  // TODO
+  const result = await db.query(
+    'INSERT INTO employees (name, birthday, salary) VALUES ($1, $2, $3) RETURNING *;',[name, birthday, salary]
+  )
 }
 
 // === Part 2 ===
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,7 @@
-import db from "#db/client";
+import dotenv from 'dotenv'
+dotenv.config()
+import db from "./client.js";
+import { createEmployee } from "./queries/employees.js";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +9,16 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  await createEmployee("Kelly", '1965-06-25', 120000)
+  await createEmployee("Mark", '1998-03-19', 100000)
+  await createEmployee("John", '1995-12-22', 110000)
+  await createEmployee("Anna", '2000-05-03', 90000)
+  await createEmployee("Russ", '1988-11-12', 90000)
+  await createEmployee("Christian", '2001-04-17', 130000)
+  await createEmployee("Nikola", '1995-02-19', 90000)
+  await createEmployee("Aaron", '1995-09-16', 90000)
+  await createEmployee("Michael", '1998-06-29', 90000)
+  await createEmployee("Jamal", '1997-02-23', 90000)
+
+ 
 }
